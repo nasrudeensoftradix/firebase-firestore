10a980518df65f8f18ed70d3d3e11ced6845b1a8
nasrudeen ansari <nasrudeen@softradix.com>
2023-08-18 11:04:28 +0530

    Add Delete and Update functionality

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,42 +1,61 @@
 import React, { useEffect, useState } from "react";
-import { collection, getDocs, onSnapshot, doc } from "firebase/firestore";
+import {
+  collection,
+  orderBy,
+  getDocs,
+  onSnapshot,
+  query,
+} from "firebase/firestore";
 import { db } from "../firebase";
 
-export default function Table() {
+export default function Table({ setTodo, handleDelete }) {
   const [todos, setTodos] = useState([]);
 
-  const fetchPost = async () => {
-    const querySnapshot = await getDocs(collection(db, "users"));
-    let res = querySnapshot?.docs?.map((doc) => ({
-      ...doc.data(),
-    }));
-    console.log(res, 5645445);
-    setTodos(res || []);
-  };
-
+  /* function to get all item from firestore in realtime */
   useEffect(() => {
-    fetchPost();
-  }, [db]);
-
-  useEffect(() => {
-    const unsubscribe = onSnapshot(
-      doc(db, "users", "documentId"),
-      (snapshot) => {
-        // setData(snapshot.data());
-        console.log(snapshot, "544554");
-      }
-    );
-
-    return () => {
-      unsubscribe();
-    };
+    const taskColRef = query(collection(db, "users"));
+    onSnapshot(taskColRef, (snapshot) => {
+      setTodos(
+        snapshot.docs.map((doc) => ({
+          id: doc.id,
+          data: doc.data(),
+        }))
+      );
+    });
   }, []);
 
   return (
     <div>
-      {todos?.map((todo, i) => (
-        <p key={i}>{todo.first}</p>
-      ))}
+      <table>
+        <tr>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Middle Name</th>
+          <th>Born</th>
+          <th>Action</th>
+        </tr>
+        {todos?.map((todo, i) => {
+          const { first, last, middle, born } = todo?.data;
+          return (
+            <tr key={todo.id}>
+              <td>{first}</td>
+              <td>{last}</td>
+              <td>{middle}</td>
+              <td>{born}</td>
+              <td>
+                <div className="action_btn">
+                  <button
+                    onClick={() => setTodo({ ...todo?.data, id: todo.id })}
+                  >
+                    Edit
+                  </button>
+                  <button onClick={() => handleDelete(todo.id)}>Delete</button>
+                </div>
+              </td>
+            </tr>
+          );
+        })}
+      </table>
     </div>
   );
 }
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,48 +1,125 @@
 import React, { useState } from "react";
-import { collection, addDoc } from "firebase/firestore";
+import {
+  collection,
+  doc,
+  updateDoc,
+  deleteDoc,
+  addDoc,
+} from "firebase/firestore";
 import Table from "./Table";
 import { db } from "../firebase";
 
 const Todo = () => {
-  const [todo, setTodo] = useState("");
+  //States
+  const [todo, setTodo] = useState({
+    first: "",
+    middle: "",
+    last: "",
+    born: "",
+  });
 
+  //Destructur
+  const { first, last, born, middle } = todo;
+
+  //onchange handler
+  const onChangeHandler = (e) => {
+    const { name, value } = e.target;
+    setTodo((prev) => ({ ...prev, [name]: value }));
+  };
+
+  //Add item
   const addTodo = async (e) => {
+    e.preventDefault();
     try {
-      const docRef = await addDoc(collection(db, "users"), {
-        first: "Alan2 new",
-        middle: "Mathison",
-        last: "Turing",
-        born: 1912,
+      let docRef = "";
+      if (todo?.id) {
+        const todoDocRef = doc(db, "users", todo.id);
+        docRef = await updateDoc(todoDocRef, todo);
+      } else {
+        docRef = await addDoc(collection(db, "users"), todo);
+      }
+      setTodo({
+        first: "",
+        middle: "",
+        last: "",
+        born: "",
       });
-      console.log("Document written with ID: ", docRef.id);
     } catch (e) {
       console.error("Error adding document: ", e);
+    }
+  };
+
+  /* function to delete a document from firstore */
+  const handleDelete = async (id) => {
+    const taskDocRef = doc(db, "users", id);
+    try {
+      await deleteDoc(taskDocRef);
+    } catch (err) {
+      alert(err);
     }
   };
 
   return (
     <section className="todo-container">
       <div className="todo">
-        <h1 className="header">Todo-App</h1>
+        <h1 className="header">Firestor Todo-App</h1>
 
-        <div>
-          <div>
-            <input
-              type="text"
-              placeholder="What do you have to do today?"
-              onChange={(e) => setTodo(e.target.value)}
-            />
+        <form onSubmit={addTodo}>
+          <div className="add_container">
+            <div>
+              <label>First Name</label>
+              <input
+                type="text"
+                placeholder="What do you have to do today?"
+                name={"first"}
+                required
+                value={first}
+                onChange={onChangeHandler}
+              />
+            </div>
+            <div>
+              <label>Last Name</label>
+              <input
+                type="text"
+                placeholder="What do you have to do today?"
+                name={"last"}
+                value={last}
+                required
+                onChange={onChangeHandler}
+              />
+            </div>
+            <div>
+              <label>Middle Name</label>
+              <input
+                type="text"
+                placeholder="What do you have to do today?"
+                name={"middle"}
+                value={middle}
+                required
+                onChange={onChangeHandler}
+              />
+            </div>
+            <div>
+              <label>Middle Name</label>
+              <input
+                type="date"
+                placeholder="What do you have to do today?"
+                name={"born"}
+                value={born}
+                required
+                onChange={onChangeHandler}
+              />
+            </div>
+            <div className="btn-container">
+              <button type="submit" className="btn">
+                {todo?.id ? "Update" : "Submit"}
+              </button>
+            </div>
           </div>
-
-          <div className="btn-container">
-            <button type="submit" className="btn" onClick={addTodo}>
-              Submit
-            </button>
-          </div>
-        </div>
+        </form>
 
         <div className="todo-content">
-          <Table />
+          <Table setTodo={setTodo} handleDelete={handleDelete} />
         </div>
       </div>
     </section>
